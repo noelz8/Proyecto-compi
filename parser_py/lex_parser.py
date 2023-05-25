023d90d9f5acfe0850cac0c7792941d700074cd0
Eder Vega <3derv@estudiantec.cr>
2023-05-25 17:29:51 -0600

    Amplacion de lex

--- parser_py/lex_parser.py
+++ parser_py/lex_parser.py
@@ -1,26 +1,144 @@
 import ply.lex as lex
-
+import re
 # Lista de nombres de tokens
 tokens = [
-    'ID',
-    'INTEGER',
-    'PLUS',
-    'EQUAL',
-    'LPAREN',
-    'RPAREN'
+    # Alfabeto completo en español
+    'LETRA',
+    # Números
+    'NUMERO',
+    # Tildes y ñ
+    'TILDE',
+    'ENYE',
+    # Símbolos especiales
+    'SUMA',
+    'RESTA',
+    'DIVISION',
+    'MULTIPLICACION',
+    'IGUALDAD',
+    # Puntuación
+    'PUNTO',
+    'COMA',
+    'MAYUSCULA',
+    'PUNTO_COMA',
+    'COMILLAS',
+    'GUION',
+    'UNDERSCORE',
+    'ARROBA',
+    'SLASH',
+    'COMENTARIO',
+    'LPARENTESIS',
+    'RPARENTESIS',
+    'IZQ_INTERROGACION',
+    'DER_INTERROGACION',
+    'IZQ_EXCLAMACION',
+    'DER_EXCLAMACION',
+    #Condiciones numericas
+    'MAYOR_QUE',
+    'MENOR_QUE',
+    'IGUAL',
+    'DISTINTO_QUE',
+    'MAYOR_IGUAL_QUE',
+    'MENOR_IGUAL_QUE',
+    #Palabras reservadas
+    'PROC',
+    'MASTER',
+    'CALL',
+    'NEW',
+    'ALTER',
+    'ADD',
+    'SUB',
+    'MUL',
+    'DIV',
+    'ALTERB',
+    'SIGNAL',
+    'VIEWSIGNAL',
+    'ISTRUE',
+    'REPEAT',
+    'BREAK',
+    'UNTIL',
+    'WHILE',
+    'CASE',
+    'WHEN',
+    'THEN',
+    'ELSE',
+    'PRINTVALUES',
+    'VALUES',
+    'ID'
 ]
+reserved = {
+    
+    'Proc': 'PROC',
+    '@Master': 'MASTER',
+    'CALL': 'CALL',
+    'New': 'NEW',
+    'Alter': 'ALTER',
+    'ADD':'ADD',
+    'SUB':'SUB',
+    'MUL':'MUL',
+    'DIV':'DIV',
+    'AlterB':'ALTERB',
+    'Signal': 'SIGNAL',
+    'ViewSignal':'VIEWSIGNAL',
+    'IsTrue': 'ISTRUE',
+    'Repeat':'REPEAT',
+    'break': 'BREAK',
+    'Until': 'UNTIL',
+    'While': 'WHILE',
+    'Case': 'CASE',
+    'When':'WHEN',
+    'Then':'THEN',
+    'else': 'ELSE',
+    'PrintValues': 'PRINTVALUES',
+    'Values': 'VALUES'
+}
 
+cond_Numericas = {
+    '>':'MAYOR_QUE',
+    '<':'MENOR_QUE',
+    '==':'IGUAL',
+    '<>':'DISTINTO_QUE',
+    '>=':'MAYOR_IGUAL_QUE',
+    '<=':'MENOR_IGUAL_QUE'
+}
 # Definición de tokens
-t_ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-t_INTEGER = r'\d+'
-t_PLUS = r'\+'
-
-t_EQUAL = r'='
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
+t_TILDE = r'[áéíóúÁÉÍÓÚ]'
+t_LETRA = r'[a-zA-ZáéíóúÁÉÍÓÚüÜ]'
+t_NUMERO = r'\d+'
+t_COMENTARIO = r'//'
+t_ENYE = r'ñ'
+t_SUMA = r'\+'
+t_RESTA = r'-'
+t_DIVISION = r'/'
+t_MULTIPLICACION = r'\*'
+t_IGUALDAD = r'='
+t_PUNTO = r'\.'
+t_COMA = r','
+t_MAYUSCULA = r'[A-Z]'
+t_PUNTO_COMA = r';'
+t_COMILLAS = r'\"'
+t_GUION = r'-'
+t_UNDERSCORE = r'_'
+t_ARROBA = r'@'
+t_SLASH = r'/'
+t_LPARENTESIS = r'\('
+t_RPARENTESIS = r'[\)]'
+t_IZQ_INTERROGACION = r'\¿'
+t_DER_INTERROGACION = r'\?'
+t_IZQ_EXCLAMACION = r'¡'
+t_DER_EXCLAMACION = r'!'
 
 # Ignorar caracteres en blanco
 t_ignore = ' \t'
+
+def t_COND_NUMERICA(t):
+    r'>|<|==|<>|>=|<='
+    t.type = cond_Numericas.get(t.value)
+    return t
+
+def t_RESERVED(t):
+    r'[a-zA-Z_@][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
 
 # Regla para manejar saltos de línea
 def t_newline(t):
@@ -34,3 +152,26 @@ def t_error(t):
 
 # Crear el analizador léxico
 lexer = lex.lex()
+data = '''// Nombre y funcionalidad del código
+
+Proc @Master
+(
+  
+    Values (@variable2, True);
+    While IsTrue(@variale2)
+        ( Signal(@variale2, 1);
+        AlterB (@variable2);
+);
+    Values (@variable1, 100);
+    While @variable1 > 10
+        ( Signal(@variale1, 1);
+        Values (@variable1,
+        Alter (@variable1,SUB, 10));
+ );
+);'''
+lexer.input(data)
+while True:
+    token = lexer.token()
+    if not token:
+        break
+    print(token)
--- parser_py/yacc_parser.py
+++ parser_py/yacc_parser.py
@@ -2,15 +2,110 @@
 import ply.yacc as yacc
 from lex_parser import tokens
 
-# Definición de reglas de producción
-def p_expression(p):
-    'expression : INTEGER PLUS INTEGER'
-    p[0] = p[1] + p[3]
+# Reglas de precedencia
+precedence = (
+    ('left', 'SUMA', 'RESTA'),
+    ('left', 'MULTIPLICACION', 'DIVISION'),
+)
 
-# Regla para manejar errores de sintaxis
+# Definición de la gramática
+def p_program(p):
+    '''
+    program : comentario procedimientos
+    '''
+    # Acciones semánticas correspondientes
+    # ...
+
+def p_comentario(p):
+    '''
+    comentario : COMENTARIO
+    '''
+    # Acciones semánticas correspondientes
+    # ...
+
+def p_procedimientos(p):
+    '''
+    procedimientos : procedimiento
+                   | procedimientos procedimiento
+    '''
+    # Acciones semánticas correspondientes
+    # ...
+
+def p_procedimiento(p):
+    '''
+    procedimiento : PROC ID '(' cuerpo_procedimiento ')'
+    '''
+    # Acciones semánticas correspondientes
+    # ...
+
+def p_cuerpo_procedimiento(p):
+    '''
+    cuerpo_procedimiento : instrucciones
+    '''
+    # Acciones semánticas correspondientes
+    # ...
+
+def p_instrucciones(p):
+    '''
+    instrucciones : instruccion
+                  | instrucciones instruccion
+    '''
+    # Acciones semánticas correspondientes
+    # ...
+
+def p_instruccion(p):
+    '''
+    instruccion : asignacion
+                | llamada_procedimiento
+    '''
+    # Acciones semánticas correspondientes
+    # ...
+
+def p_asignacion(p):
+    '''
+    asignacion : ID '=' expresion ';'
+    '''
+    # Acciones semánticas correspondientes
+    # ...
+
+def p_llamada_procedimiento(p):
+    '''
+    llamada_procedimiento : CALL '(' ID ')' ';'
+    '''
+    # Acciones semánticas correspondientes
+    # ...
+
+def p_expresion(p):
+    '''
+    expresion : NUMERO
+              | ID
+              | expresion SUMA expresion
+              | expresion RESTA expresion
+              | expresion MULTIPLICACION expresion
+              | expresion DIVISION expresion
+    '''
+    # Acciones semánticas correspondientes
+    # ...
+
+# Manejo de errores de análisis sintáctico
 def p_error(p):
-    print("Error de sintaxis en la entrada")
+    print("Error de sintaxis:", p)
 
-# Crear el analizador sintáctico
+# Construir el parser
 parser = yacc.yacc()
 
+# Prueba del parser
+data = '''
+// Nombre y funcionalidad del código
+
+Proc @Master
+(
+  // Instrucciones
+);
+
+Proc nombre_del_procedimiento
+(
+  // Instrucciones
+);
+'''
+parser.parse(data)
